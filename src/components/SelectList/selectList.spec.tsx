105c0616a9c002d067a0ac2316649b3b91b3d1b1
arthur <arthur.rios007@gmail.com>
2024-02-12 23:47:31 -0300

    chore: add select list tests

---
+++ src/components/SelectList/selectList.spec.tsx
@@ -0,0 +1,27 @@
+import { fireEvent, render, screen } from '@testing-library/react-native'
+import { SelectList } from '@components/SelectList'
+
+describe('Component: SelectList', () => {
+  it('should be returned with selected city details.', () => {
+    const data = [
+      { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+      { id: '2', name: 'Campo Grande', latitude: 789, longitude: 987 },
+    ]
+
+    const onPress = jest.fn()
+
+    render(<SelectList data={data} onChange={() => {}} onPress={onPress} />)
+
+    const selectedCity = screen.getByText(/campo/i)
+    fireEvent.press(selectedCity)
+
+    expect(onPress).toHaveBeenCalledWith(data[1])
+  })
+
+  it('should not show options when data props is empty.', () => {
+    render(<SelectList data={[]} onChange={() => {}} onPress={() => {}} />)
+
+    const options = screen.getByTestId('options')
+    expect(options.children).toHaveLength(0)
+  })
+})
