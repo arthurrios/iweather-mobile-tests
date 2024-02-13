8a056d8b6f9c1ce5a42c0be3a8f3ac5edae6a657
arthur <arthur.rios007@gmail.com>
2024-02-12 23:23:45 -0300

    chore: add input component test

---
+++ src/components/Input/input.spec.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react-native'
+import { Input } from '@components/Input'
+
+describe('Component: Input', () => {
+  it('should render without activity indicator if isLoading prop is undefined.', () => {
+    render(<Input />)
+
+    const activityIndicator = screen.queryByTestId('activity-indicator')
+    expect(activityIndicator).toBeNull()
+  })
+
+  it('should render without activity indicator if isLoading prop is true.', () => {
+    render(<Input isLoading />)
+
+    const activityIndicator = screen.queryByTestId('activity-indicator')
+    expect(activityIndicator).toBeTruthy()
+  })
+})
